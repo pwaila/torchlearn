671df27ee8651cef5d9e1fee5896cb572d5d33e7
Pranav Waila <pranav.waila@gmail.com>
2018-12-21 22:58:50 +0530

    Update mnist_data_prepration.py

--- data_prepration/mnist_data_prepration.py
+++ data_prepration/mnist_data_prepration.py
@@ -1,7 +1,7 @@
 
 '''
-@author: Pranav
 File path : /home/pune1/pranav/Deeplearning/data/mnist/data/
+#taking from: https://github.com/Ghosh4AI/Data-Processors/tree/master/MNIST
 '''
 
 
@@ -14,26 +14,6 @@ from skimage.io import imsave
 # PROVIDE YOUR DOWNLOAD DIRECTORY HERE
 datapath = '/home/pune1/pranav/Deeplearning/data/mnist/data/'  
 
-# LISTING ALL ARCHIVES IN THE DIRECTORY
-files = os.listdir(datapath)
-for file in files:
-    if file.endswith('gz'):
-        print('Extracting ',file)
-        with gzip.open(datapath+file, 'rb') as f_in:
-            with open(datapath+file.split('.')[0], 'wb') as f_out:
-                shutil.copyfileobj(f_in, f_out)
-print('Extraction Complete')
-
-# OPTIONAL REMOVE THE ARCHIVES
-for file in files:
-    print('Removing ',file)
-    os.remove(datapath+file)
-print ('All archives removed')
-
-
-
-
-
 files = os.listdir(datapath)
 
 def get_int(b):   # CONVERTS 4 BYTES TO A INT
@@ -41,35 +21,30 @@ def get_int(b):   # CONVERTS 4 BYTES TO A INT
 
 data_dict = {}
 for file in files:
-    if file.endswith('ubyte'):  # FOR ALL 'ubyte' FILES
-        print('Reading ',file)
-        with open (datapath+file,'rb') as f:
-            data = f.read()
-            type = get_int(data[:4])   # 0-3: THE MAGIC NUMBER TO WHETHER IMAGE OR LABEL
-            length = get_int(data[4:8])  # 4-7: LENGTH OF THE ARRAY  (DIMENSION 0)
-            if (type == 2051):
-                category = 'images'
-                num_rows = get_int(data[8:12])  # NUMBER OF ROWS  (DIMENSION 1)
-                num_cols = get_int(data[12:16])  # NUMBER OF COLUMNS  (DIMENSION 2)
-                parsed = numpy.frombuffer(data,dtype = numpy.uint8, offset = 16)  # READ THE PIXEL VALUES AS INTEGERS
-                parsed = parsed.reshape(length,num_rows,num_cols)  # RESHAPE THE ARRAY AS [NO_OF_SAMPLES x HEIGHT x WIDTH]           
-            elif(type == 2049):
-                category = 'labels'
-                parsed = numpy.frombuffer(data, dtype=numpy.uint8, offset=8) # READ THE LABEL VALUES AS INTEGERS
-                parsed = parsed.reshape(length)  # RESHAPE THE ARRAY AS [NO_OF_SAMPLES]                           
-            if (length==10000):
-                set = 'test'
-            elif (length==60000):
-                set = 'train'
-            data_dict[set+'_'+category] = parsed  # SAVE THE NUMPY ARRAY TO A CORRESPONDING KEY
+    print('Reading ',file)
+    with open (datapath+file,'rb') as f:
+        data = f.read()
+        type = get_int(data[:4])   # 0-3: THE MAGIC NUMBER TO WHETHER IMAGE OR LABEL
+        length = get_int(data[4:8])  # 4-7: LENGTH OF THE ARRAY  (DIMENSION 0)
+        if (type == 2051):
+            category = 'images'
+            num_rows = get_int(data[8:12])  # NUMBER OF ROWS  (DIMENSION 1)
+            num_cols = get_int(data[12:16])  # NUMBER OF COLUMNS  (DIMENSION 2)
+            parsed = numpy.frombuffer(data,dtype = numpy.uint8, offset = 16)  # READ THE PIXEL VALUES AS INTEGERS
+            parsed = parsed.reshape(length,num_rows,num_cols)  # RESHAPE THE ARRAY AS [NO_OF_SAMPLES x HEIGHT x WIDTH]           
+        elif(type == 2049):
+            category = 'labels'
+            parsed = numpy.frombuffer(data, dtype=numpy.uint8, offset=8) # READ THE LABEL VALUES AS INTEGERS
+            parsed = parsed.reshape(length)  # RESHAPE THE ARRAY AS [NO_OF_SAMPLES]                           
+        if (length==10000):
+            set = 'test'
+        elif (length==60000):
+            set = 'train'
+        data_dict[set+'_'+category] = parsed  # SAVE THE NUMPY ARRAY TO A CORRESPONDING KEY
 
-
-import os
-from skimage.io import imsave
+#for storing images and keeping the dictionary containing images and labels
 datapath = '/home/pune1/pranav/Deeplearning/MNISTData/' # PATH WHERE IMAGES WILL BE SAVED
-
 sets = ['train','test']
-
 for set in sets:   # FOR TRAIN AND TEST SET
     images = data_dict[set+'_images']   # IMAGES
     labels = data_dict[set+'_labels']   # LABELS
@@ -83,8 +58,8 @@ for set in sets:   # FOR TRAIN AND TEST SET
         filenumber = len(os.listdir(datapath+set+'/'+str(label)+'/'))  # NUMBER OF FILES IN THE DIRECTORY FOR NAMING THE FILE
         imsave(datapath+set+'/'+str(label)+'/%05d.png'%(filenumber),image)  # SAVE THE IMAGE WITH PROPER NAME
 
+#keeping a dictionary having all the data, to be used later
 import pickle
-
 datapath = '/home/pune1/pranav/Deeplearning/MNISTData/'
 
 # DUMPING THE DICTIONARY INTO A PICKLE 
